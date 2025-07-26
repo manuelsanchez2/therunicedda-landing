400d9922024c8acd8426d22301ddaf51069d961d
Manuel Sanchez <manusansan22@gmail.com>
2025-07-26 16:55:52 +0200

    feat: Add character generation script and individual character pages

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,22 +1,24 @@
-import { EleventyHtmlBasePlugin, IdAttributePlugin } from '@11ty/eleventy'
-import syntaxHighlight from '@11ty/eleventy-plugin-syntaxhighlight'
-import metadata from './src/_data/metadata.js'
+import { EleventyHtmlBasePlugin, IdAttributePlugin } from "@11ty/eleventy"
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight"
+import metadata from "./src/_data/metadata.js"
 
 export default async function (eleventyConfig) {
-  eleventyConfig.setInputDirectory('src')
-  eleventyConfig.setOutputDirectory('docs')
+  eleventyConfig.setInputDirectory("src")
+  eleventyConfig.setOutputDirectory("docs")
 
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin)
   eleventyConfig.addPlugin(IdAttributePlugin)
   eleventyConfig.addPlugin(syntaxHighlight)
 
   eleventyConfig.addPassthroughCopy({
-    'src/public/': '/',
+    "src/public/": "/",
   })
+
+  eleventyConfig.addWatchTarget("src/public")
 }
 
 export const config = {
   pathPrefix: metadata.pathPrefix,
-  markdownTemplateEngine: 'njk',
-  htmlTemplateEngine: 'njk',
+  markdownTemplateEngine: "njk",
+  htmlTemplateEngine: "njk",
 }
